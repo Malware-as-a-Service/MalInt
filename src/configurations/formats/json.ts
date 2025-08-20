ae9e9f23600f7ff95091877b2243a1645d8d44db
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-20 09:40:05 +0200

    feat: ✨ Add the JSON format

---
+++ src/configurations/formats/json.ts
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2025 The MalInt development team
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { Format } from ".";
+import { Repository } from "../types";
+import { FailToParse } from "../errors";
+import { Result, safeTry, err, ok } from "neverthrow";
+import { z, ZodError, safeParse } from "zod";
+
+export class Json implements Format {
+  deserializeRepository(
+    content: string,
+  ): Result<z.infer<typeof Repository>, FailToParse | ZodError> {
+    return safeTry(function* () {
+      const parsedContent = yield* Result.fromThrowable(
+        JSON.parse,
+        () => new FailToParse(),
+      )(content);
+
+      const result = safeParse(Repository, parsedContent);
+
+      if (!result.success) {
+        return err(result.error);
+      }
+
+      return ok(result.data);
+    });
+  }
+}
