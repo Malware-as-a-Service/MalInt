5f37a8127c20d884dbd1e462eea3dd85552f8dae
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-20 15:13:40 +0200

    feat: ✨ Update signatures

--- src/forges/index.ts
+++ src/forges/index.ts
@@ -3,20 +3,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import { Result } from "neverthrow";
+import { FileNotFound, NotAFile, VariableNotFound } from "./errors";
 
 export type Repository = {
-  baseAddress: URL;
+  baseAddress: string;
   ownerUsername: string;
   name: string;
 };
 
 export interface Forge {
-  getVariable(name: string): Promise<Result<string, Error>>;
-  getSecret(name: string): Promise<Result<string, Error>>;
-  getContent(path: string): Promise<Result<string, Error>>;
+  getVariable(name: string): Promise<Result<string, VariableNotFound | Error>>;
+  getContent(
+    path: string,
+  ): Promise<Result<string, FileNotFound | NotAFile | Error>>;
   writeContent(
     path: string,
+    branch: string,
     message: string,
     content: string,
-  ): Promise<Result<string, Error>>;
+  ): Promise<Result<void, FileNotFound | NotAFile | Error>>;
 }
