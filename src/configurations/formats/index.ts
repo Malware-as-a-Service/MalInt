92fa31d79317b2080fd87c3830bbbc61409ab181
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-20 09:20:01 +0200

    feat: ✨ Update the signature of the deserializeRepository function

--- src/configurations/formats/index.ts
+++ src/configurations/formats/index.ts
@@ -1,17 +1,14 @@
+// SPDX-FileCopyrightText: 2025 The MalInt development team
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import { Result } from "neverthrow";
 import { Repository } from "../types";
-import {
-  FailToParse,
-  MissingKey,
-  MissingSection,
-  WrongKeyType,
-} from "../errors";
+import { FailToParse } from "../errors";
+import { z, ZodError } from "zod";
 
 export interface Format {
-  deserializeRepository(
-    content: string,
-  ): Result<
-    Repository,
-    FailToParse | MissingSection | MissingKey | WrongKeyType
-  >;
+	deserializeRepository(
+		content: string,
+	): Result<z.infer<typeof Repository>, FailToParse | ZodError>;
 }
