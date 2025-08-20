afb9bde8b1c49579f036924af88142801aa6c2fa
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-20 09:19:14 +0200

    feat: ✨ Simplify the errors

--- src/configurations/errors.ts
+++ src/configurations/errors.ts
@@ -4,34 +4,8 @@
 
 export class FailToParse extends Error {
   constructor() {
-    super(`Fail to parse the content.`);
+    super("Fail to parse the content.");
 
     this.name = "FailToParse";
-  }
-}
-
-export class MissingSection extends Error {
-  constructor(section: string) {
-    super(`Section "${section}" is missing.`);
-
-    this.name = "SectionMissing";
-  }
-}
-
-export class MissingKey extends Error {
-  constructor(key: string) {
-    super(`Key "${key}" is missing.`);
-
-    this.name = "KeyMissing";
-  }
-}
-
-export class WrongKeyType extends Error {
-  constructor(key: string, expectedType: string, currentType: string) {
-    super(
-      `Key "${key}" is of the wrong type, expected ${expectedType} but found ${currentType}.`,
-    );
-
-    this.name = "WrongKeyType";
   }
 }
