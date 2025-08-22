c9d5f22a1e5f352efcce97a826a17c0e65b7c98f
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-22 09:09:13 +0200

    style: 💄 Add missing SPDX header

--- src/validator/errors.ts
+++ src/validator/errors.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 The MalInt development team
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 export class InvalidVariable extends Error {
   constructor(name: string) {
     super(
