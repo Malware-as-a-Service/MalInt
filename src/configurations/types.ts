8a783be43c664f1a8db3e72e0a88a419becc5fba
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-22 10:33:17 +0200

    refactor: 🔨 Merge every type into the Repository as we don't just use the other one and this reduce naming collisions

--- src/configurations/types.ts
+++ src/configurations/types.ts
@@ -8,69 +8,57 @@ const variableNameRegex = /^(?!FORGEJO_|GITHUB_)[A-Za-z0-9_]+$/;
 const variableNameErrorMessage =
 	'Variable name must not start with "FORGEJO_" or "GITHUB_" and can only contain alphanumeric characters and underscores.';
 
-export const Forge = z.object({
-	buildingBranchVariableName: z
-		.string()
-		.uppercase()
-		.regex(variableNameRegex, variableNameErrorMessage),
-	configurationPathVariableName: z
-		.string()
-		.regex(variableNameRegex, variableNameErrorMessage),
-});
-
-export const ServerConfigurations = z.object({
-	serverConfigurationPath: z.string().optional(),
-	malwareConfigurationPath: z.string(),
-});
-
-export const ClientConfigurations = z
-	.object({
-		serverConfigurationPath: z.string().optional(),
-		serverUiPath: z.string().optional(),
-		malwareConfigurationPath: z.string().optional(),
-		malwareUiPath: z.string().optional(),
-	})
-	.refine(
-		(configuration) =>
-			(configuration.serverConfigurationPath === undefined &&
-				configuration.serverUiPath === undefined) ||
-			(configuration.serverConfigurationPath !== undefined &&
-				configuration.serverUiPath !== undefined),
-		{
-			message:
-				'The "serverConfigurationPath" and "serverUiPath" fields must be present, or both undefined.',
-			path: ["serverConfigurationPath", "serverUiPath"],
-		},
-	)
-	.refine(
-		(configuration) =>
-			(configuration.malwareConfigurationPath === undefined &&
-				configuration.malwareUiPath === undefined) ||
-			(configuration.malwareConfigurationPath !== undefined &&
-				configuration.malwareUiPath !== undefined),
-		{
-			message:
-				'Fields "The "malwareConfigurationPath" and "malwareUiPath" fields must be present, or both undefined.',
-			path: ["mawlareConfigurationPath", "malwareUiPath"],
-		},
-	);
-
-export const Configurations = z.object({
-	server: ServerConfigurations,
-	client: ClientConfigurations,
-});
-
-export const Server = z.object({
-	containerfilePath: z.string(),
-});
-
-export const Malware = z.object({
-	configurationPath: z.string(),
-});
-
 export const Repository = z.object({
-	forge: Forge,
-	configurations: Configurations,
-	server: Server,
-	malware: Malware,
+	forge: z.object({
+		buildingBranchVariableName: z
+			.string()
+			.uppercase()
+			.regex(variableNameRegex, variableNameErrorMessage),
+		configurationPathVariableName: z
+			.string()
+			.regex(variableNameRegex, variableNameErrorMessage),
+	}),
+	server: z.object({
+		containerfilePath: z.string(),
+	}),
+	malware: z.object({
+		configurationPath: z.string(),
+	}),
+	configurations: z.object({
+		serverSide: z.object({
+			serverConfigurationPath: z.string().optional(),
+			malwareConfigurationPath: z.string(),
+		}),
+		clientSide: z
+			.object({
+				serverConfigurationPath: z.string().optional(),
+				serverUiPath: z.string().optional(),
+				malwareConfigurationPath: z.string().optional(),
+				malwareUiPath: z.string().optional(),
+			})
+			.refine(
+				(configuration) =>
+					(configuration.serverConfigurationPath === undefined &&
+						configuration.serverUiPath === undefined) ||
+					(configuration.serverConfigurationPath !== undefined &&
+						configuration.serverUiPath !== undefined),
+				{
+					message:
+						'The "serverConfigurationPath" and "serverUiPath" fields must be present, or both undefined.',
+					path: ["serverConfigurationPath", "serverUiPath"],
+				},
+			)
+			.refine(
+				(configuration) =>
+					(configuration.malwareConfigurationPath === undefined &&
+						configuration.malwareUiPath === undefined) ||
+					(configuration.malwareConfigurationPath !== undefined &&
+						configuration.malwareUiPath !== undefined),
+				{
+					message:
+						'Fields "The "malwareConfigurationPath" and "malwareUiPath" fields must be present, or both undefined.',
+					path: ["mawlareConfigurationPath", "malwareUiPath"],
+				},
+			),
+	}),
 });
