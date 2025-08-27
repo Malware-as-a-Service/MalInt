7eaea024c7e7d99fcbbcb31bdac674a103f65cf9
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-08-27 15:21:39 +0200

    build: 📦 Move tsdown config to it's own file

---
+++ tsdown.config.ts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 The MalInt development team
+//
+// SPDX-License-Identifier: CC0-1.0
+
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+    entry: "src/index.ts",
+    dts: true,
+    clean: true,
+    format: [
+        "esm",
+        "cjs"
+    ],
+});
