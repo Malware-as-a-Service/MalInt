2d02aee50d18ea66b6907fdcbf797679abc78138
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2025-09-03 11:14:23 +0200

    feat: ✨ Rework the errors

--- src/api/errors.ts
+++ src/api/errors.ts
@@ -2,43 +2,12 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import type { Result } from "neverthrow";
+import { ZodError } from "zod";
 
-export type OkType<T> = T extends Result<infer U, unknown> ? U : never;
-export type ErrorType<T> = T extends Result<unknown, infer U> ? U : never;
-export type Invoke<T> = (
-	| FunctionNotFound
-	| WrongNumberOfArguments
-	| InvalidArgumentType
-	| ErrorType<T>
-)[];
+export type Invoke = FunctionNotFound | ZodError;
 
 export interface FunctionNotFound {
-	type: "functionNotFound";
-	message: string;
-	name: string;
-}
-
-export interface WrongNumberOfArguments {
-	type: "wrongNumberOfArguments";
-	message: string;
-	functionName: string;
-	provided: number;
-	expected: number;
-}
-
-export interface InvalidArgumentType {
-	type: "invalidArgumentType";
-	message: string;
-	functionName: string;
-	expected: string;
-}
-
-export interface InvalidArgument {
-	type: "invalidArgument";
-	message: string;
-	functionName: string;
-	name: string;
-	expectedType: string;
-	value: unknown;
+  type: "functionNotFound";
+  message: string;
+  name: string;
 }
